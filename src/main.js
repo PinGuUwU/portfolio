c7ea5e264d21bb2e8f95ce699e58c7a3bd5c6d38
Priscila Redondo <prisredondo29@gmail.com>
2025-09-23 16:17:57 -0300

    Creando una nueva página desde cero, con nav, presentación, sobre mi y valores, proximamente más

--- src/main.js
+++ src/main.js
@@ -1,27 +1,2 @@
 import './style.css'
 
-const toggle = document.querySelector(".hamburguesa")
-const nav = document.querySelector("nav")
-const up = document.querySelector("#up")
-
-
-const htmltemp = document.documentElement; 
-// cambiamos
-htmltemp.setAttribute('data-theme', 'light');
-
-
-// ABRIR BARRA DE NAVEGACIÓN
-
-toggle.addEventListener(
-    'click', () => {
-        nav.classList.toggle('show')
-    }
-)
-
-up.addEventListener(
-    'click', () => {
-        scrollTo(0)
-    }
-)
-
-// CAMBIO DE TEMA
--- vite.config.js
+++ vite.config.js
@@ -3,3 +3,4 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/portfolio/', // rutas relativas para que funcione en GitHub Pages
 })
+
